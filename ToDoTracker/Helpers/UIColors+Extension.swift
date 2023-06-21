5802a434700819e2793c6fce926f1f9b9a8c7112
Denis Chakyr <ChakyrIT@gmail.com>
2023-06-21 12:05:07 +0300

    Added LaunchScreen

---
+++ ToDoTracker/Helpers/UIColors+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColors+Extension.swift
+//  ToDoTracker
+//
+//  Created by Денис on 16.06.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let YPBackground = UIColor(named: "YPBackground")
+    static let YPRed = UIColor(named: "YPRed")
+    static let YPWhite = UIColor(named: "YPWhite")
+    static let YPBlack = UIColor(named: "YPBlack")
+    static let YPBlue = UIColor(named: "YPBlue")
+    static let YPGrey = UIColor(named: "YPGrey")
+    static let YPLightGrey = UIColor(named: "YPLightGrey")
+}
+
+//ПОЗЖЕ СЮДА ДОБАВИТЬ КУЧУ ЦВетов
--- ToDoTracker/ViewController.swift
+++ ToDoTracker/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    //MARK: - Private Properties
+    
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    //MARK: -LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .YPBlack
+        
+        collectionView.delegate = self
+        
+        createCollectionView()
+        collectionView.dataSource = self
+        ///Регистрируем ячейку
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
-
-
+    
+    //MARK: -Private Methods
+    ///Создаем UICollectionView
+    private func createCollectionView() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+    
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
 }
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 5.0, height: 5.0)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return CGFloat(5.0)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return CGFloat(5.0)
+//    }
+}
+
+//MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = .YPRed
+        return cell
+    }
+}
+
 
