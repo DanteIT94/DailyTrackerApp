888d5eaa5d4808cefc473352728c2ab82a78fc25
Denis Chakyr <ChakyrIT@gmail.com>
2023-07-09 14:53:13 +0300

    Update TrackersViewController.swift

--- ToDoTracker/TrackerViewFolder/TrackersViewController.swift
+++ ToDoTracker/TrackerViewFolder/TrackersViewController.swift
@@ -84,7 +84,7 @@ final class TrackersViewController: UIViewController {
         return textPlaceholder
     }()
     
-    private var categories: [TrackerCategory] = MockData.MockCategories.categories
+    private var categories: [TrackerCategory] = []
     //MockData.MockCategories.categories
     private var visibleCategories: [TrackerCategory] = []
     private var completedTrackers: Set<TrackerRecord> = []
