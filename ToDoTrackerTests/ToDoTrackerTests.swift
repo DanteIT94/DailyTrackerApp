60fd20380be0457adee064e00d407602f71b13f9
Denis Chakyr <ChakyrIT@gmail.com>
2023-08-15 23:41:54 +0300

    Added ScreenShot-test for DarkTheme

--- ToDoTrackerTests/ToDoTrackerTests.swift
+++ ToDoTrackerTests/ToDoTrackerTests.swift
@@ -10,37 +10,40 @@ import SnapshotTesting
 @testable import ToDoTracker
 
 final class ToDoTrackerTests: XCTestCase {
-    
+        
     func testViewController() {
-        //TODO: - Знаю что нужно делать заглушки но горят сроки!
-        let trackerContainer = TrackerPersistentContainer()
-        let trackerStore = TrackerStore(context: trackerContainer.context)
-        let trackerCategoryStore = TrackerCategoryStore(
-            context: trackerContainer.context,
-            trackerDataStore: trackerStore)
-        let trackerRecordStore = TrackerRecordStore(context: trackerContainer.context)
-        
-        let categoryViewModel = CategoryViewModel(
-            trackerCategoryStore: trackerCategoryStore)
-        
-        let trackerDataController = TrackerDataController(
-            trackerCategoryStore: trackerCategoryStore,
-            trackerStore: trackerStore,
-            trackerRecordStore: trackerRecordStore,
-            context: trackerContainer.context)
-        
-        let trackersViewController = TrackersViewController(
-            trackerDataController: trackerDataController,
-            trackerCategoryStore: trackerCategoryStore,
-            categoryViewModel: categoryViewModel,
-            trackerStore: trackerStore,
-            trackerRecordStore: trackerRecordStore)
-        
-        let vc = TrackersViewController(trackerDataController: trackerDataController, trackerCategoryStore: trackerCategoryStore, categoryViewModel: categoryViewModel, trackerStore: trackerStore, trackerRecordStore: trackerRecordStore)
-        
-        assertSnapshot(matching: vc, as: .image)
-        
-        
-    }
+            //TODO: - Знаю что нужно делать заглушки, но горят сроки!
+            let trackerContainer = TrackerPersistentContainer()
+            let trackerStore = TrackerStore(context: trackerContainer.context)
+            let trackerCategoryStore = TrackerCategoryStore(
+                context: trackerContainer.context,
+                trackerDataStore: trackerStore)
+            let trackerRecordStore = TrackerRecordStore(context: trackerContainer.context)
+
+            let categoryViewModel = CategoryViewModel(
+                trackerCategoryStore: trackerCategoryStore)
+
+            let trackerDataController = TrackerDataController(
+                trackerCategoryStore: trackerCategoryStore,
+                trackerStore: trackerStore,
+                trackerRecordStore: trackerRecordStore,
+                context: trackerContainer.context)
+
+            let appMetric = AppMetrics()
+
+            let trackersViewController = TrackersViewController(
+                trackerDataController: trackerDataController,
+                trackerCategoryStore: trackerCategoryStore,
+                categoryViewModel: categoryViewModel,
+                trackerStore: trackerStore,
+                trackerRecordStore: trackerRecordStore,
+                appMetrics: appMetric)
+
+            // Для светлой темы
+            assertSnapshot(matching: trackersViewController, as: .image(traits: .init(userInterfaceStyle: .light)))
+
+            // Для темной темы
+            assertSnapshot(matching: trackersViewController, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "DarkTheme")
+        }
     
 }
